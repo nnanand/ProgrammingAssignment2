b0d636666f22f11d063cba65b2c7beb9bec672e7
nnanand <nirbhay1502122@gmail.com>
2018-07-08 12:44:25 +0530

    This is Programming assignment 2 submission R file.
    This file contains R code to inverse a square matrix. If the value is in cache then the value from cache is returned else the value is calculated and returned.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function returns a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	i <- NULL
+	set <- function(y){
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inverse) i <<- inverse
+	getInverse <- function() i
+	list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i <- x$getInverse()
+	if(!is.null(i)){
+		message("getting cached data")
+		return(i)
+	}
+	data <- x$get()
+	i <- solve(data,...)
+	x$setInverse(i)
+	i
 }
